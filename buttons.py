f412167ee96161cf7a8944d40d59b2878aeb53eb
LogikaMP <logika690@gmail.com>
2026-04-19 16:19:38 +0300

    gg

--- buttons.py
+++ buttons.py
@@ -31,7 +31,7 @@ class Sprite:
 
 class Button(Sprite):  # Створюємо клас кнопки, який наслідує властивості класу Sprite
      
-     def __init__(self, x, y, w, h, color, text, color_text, command):
+     def __init__(self, x, y, w, h, color, text, color_text, command,image = None):
           # Конструктор класу. Виконується при створенні об'єкта кнопки
 
           # Викликаємо конструктор батьківського класу Sprite і передаємо координати, розміри та колір
@@ -40,8 +40,12 @@ class Button(Sprite):  # Створюємо клас кнопки, який на
           # Зберігаємо колір тексту кнопки
           self.command = command
           # Зберігаємо функцію, яка виконається при натисканні кнопки
-          self.add_text(text)
-          # Викликаємо метод створення тексту на кнопці
+          if image:
+               self.image = pygame.image.load(image)
+               self.image = pygame.transform.scale(self.image,(w,h))
+          else:
+               self.add_text(text)
+               # Викликаємо метод створення тексту на кнопці
           self.was_pressed = False
           # Прапорець, що показує чи була кнопка натиснута раніше
 
@@ -66,13 +70,16 @@ class Button(Sprite):  # Створюємо клас кнопки, який на
           # Обчислюємо координату Y для центрування тексту по вертикалі
 
 
-     def draw(self, surface):  
-          # Метод відмалювання кнопки на екрані
-          super().draw(surface)
-          pygame.draw.rect(surface,(0,0,0),self.rect,width=5)
-          # Малюємо саму кнопку через метод батьківського класу
-          surface.blit(self.text, (self.text_x, self.text_y))
-          # Малюємо текст поверх кнопки
+     def draw(self, surface):
+          if self.image:
+               surface.blit(self.image,self.rect)
+          else:  
+               # Метод відмалювання кнопки на екрані
+               super().draw(surface)
+               pygame.draw.rect(surface,(0,0,0),self.rect,width=5)
+               # Малюємо саму кнопку через метод батьківського класу
+               surface.blit(self.text, (self.text_x, self.text_y))
+               # Малюємо текст поверх кнопки
           
      def is_clicked(self):  
           # Метод перевіряє чи натиснута кнопка
