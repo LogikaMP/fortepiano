584dfaf5682466393af7f249c9e7fe6febc67981
LogikaMP <logika690@gmail.com>
2026-04-19 15:53:20 +0300

    jj

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 '''Main – запуск гри та обробка подій'''
 # 6. Імпортуємо все що необхідно для роботи гри
 import pygame
-from settings import GRAY, WINDOW_WIDTH, WINDOW_HEIGHT, WHITE,GRAY,BLUE ,KEY
+from settings import GRAY, WINDOW_WIDTH, WINDOW_HEIGHT, WHITE,GRAY,BLUE ,KEYS
 from keys import create_keys, draw_keys
-from sounds import load_sound
+from sounds import load_sounds
 '''Додай імопрт класу меню'''
 from ui.settingsUi import SettingsMenu
 # 7. Ініцилізація та Створити вікно 
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+setting = SettingsMenu(20,20,100,40,GRAY,WHITE,BLUE)
 # 8. Створити список ректів - клавіш
-keys_rect = create_keys()
+keys_rect = create_keys(setting.num_keys)
 # 9. Створити порожню множину - натиснуті клавіши
 keys_pressed = set()
 # 10. Створити список звуків - завантажити звуки нот
-keys_sounds = load_sound()
+keys_sounds = load_sounds()
 
 '''Створи обєкт меню:
 координати - 20,20,
 розмір - 100, 40
 кольри - GREY, WHITE, BLUE'''
-setting = SettingsMenu(20,20,100,40,GRAY,WHITE,BLUE)
+
 # 11. Головний цикл гри:
 run = True
 while run:
@@ -29,13 +30,17 @@ while run:
    for event in pygame.event.get():
       '''виклич метод оновлення меню - передай подію event'''
       setting.update(event)
+      if len (keys_rect) != setting.num_keys:
+         keys_rect = create_keys(setting.num_keys)
       if event.type == pygame.QUIT:
          run = False
 #  - обробка подій (натискання та відпускання клавіш)
       if event.type == pygame.KEYDOWN and setting.game_part=="game":
          key_name = pygame.key.name(event.key)
-         keys_sounds [key_name].play()
-         keys_pressed.add(key_name)
+         if key_name in keys_rect:
+            keys_sounds[key_name].set_volume(setting.volume)
+            keys_sounds [key_name].play()
+            keys_pressed.add(key_name)
       if event.type == pygame.KEYUP:
          key_name = pygame.key.name(event.key)
          keys_pressed.discard(key_name)  
@@ -44,6 +49,7 @@ while run:
          pos = event.pos
          for key, rect in keys_rect.items():
             if rect.collidepoint(pos) and not key in keys_pressed:
+               keys_sounds[key].set_volume(setting.volume)
                keys_sounds[key].play()
                keys_pressed.add(key)
       if event.type == pygame.MOUSEBUTTONUP:
