ce92099e462555378976ae660c26d70358d60d3b
Maria Panteleichuk <logika690@gmail.com>
2026-04-23 18:21:35 +0300

    check_errot

--- effects.py
+++ effects.py
@@ -1,13 +1,13 @@
 '''Ефекти – анімація клавіш і візуальні ефекти'''
 from pygame import image,transform
 from settings import BLACK, BLUE, GRAY
-key_up = image.load("assets/images/key_unpressed.png")
-key_down = image.load("assets/images/key_unpressed.png")
+key_up = image.load("assets/data/images/key_pressed.png")
+key_down = image.load("assets/data/images/key_unpressed.png")
 def draw_effect(screen, rect, pressed):
     if pressed:
-        color = BLUE
+        img = key_up
     else:
-        color = GRAY
+        
         img = key_down
     img = transform.scale(img,(rect.w,rect.h))
     screen.blit(img,rect)
